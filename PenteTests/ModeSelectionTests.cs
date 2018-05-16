a165f5aa16348cbbe408daee5f100606902dc343
JoshuaKey <keyjoshj98@yahoo.com>
2018-05-16 10:25:46 -0600

    Added Tests

---
+++ PenteTests/ModeSelectionTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pente;
+
+namespace PenteTests {
+    [TestClass]
+    public class ModeSelectionTests {
+
+        [TestMethod]
+        public void Test() {
+
+        }
+
+    }
+}
